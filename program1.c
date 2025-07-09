725325cc7b38f128c90300d093d23a4c9900fd27
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 15:55:33 +0530

    feat: add program1.c

---
+++ program1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+      float a,b,c; 
+
+      scanf("%f",&a);
+      printf("value of a");
+      
+      scanf("%f",&b);
+      printf("value of b");
+
+      c=sqrt(pow(a,2)+pow(b,2));
+      printf("hypo %f",c);
+
+      return 0;
+
+
+}
