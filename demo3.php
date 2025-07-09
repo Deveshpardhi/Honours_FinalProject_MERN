0c6d92ced54a18d2c09f37eb26be248b409a77a0
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 16:04:17 +0530

    feat: add demo3.php

--- demo3.php
+++ demo3.php
@@ -7,6 +7,6 @@ $pythonScript = 'your_python_script.py';
 // Execute the Python script and capture its output
 $output = shell_exec("python3 $pythonScript 2>&1");
 
-// Output the captured Python script output
+// Output the captured Python script output jb
 echo '<pre>' . htmlspecialchars($output).'</pre>';
 ?>
