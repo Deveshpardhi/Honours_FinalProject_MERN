343557a682a3225c5d2e54fe5f41cc6886134ad1
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 15:55:32 +0530

    feat: add function.c

---
+++ function.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int fib();
+void main()
+{
+ int n;
+ printf("enter no of terms:");
+ scanf("%d",&n);
+ fib(n);
+// return 0;
+}
+
+int fib(int n)
+{
+  unsigned int f=0,s=1;
+  unsigned int c;
+
+  printf("fibonachi series:%d\n",0);
+  int temp;
+  do
+  {
+    c=f+s;
+    printf("%u\n",c);
+    temp=s;
+    s=c;
+    f=temp;
+    n--;
+  } while (n>1);
+  
+return ;
+}
