e64a56e9647ed436b059d7468afa4eb7a6db8dd1
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 16:04:17 +0530

    feat: add hello.c

--- hello.c
+++ hello.c
@@ -6,7 +6,7 @@ int main(int argc,char const*argv[])
     float b=8.444;
     printf("the value of a is %d and value of bis %f\n",a,b);
     printf("the value of b is %10.5f\n",b);
-    printf("the value of b is %-10.5f here\n",b);
+    printf("the value o0f b is %-10.5f here\n",b);
     printf("the value od Pi is %f\n",PI);
 
     return 0;
