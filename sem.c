debd092ffde7eaec28d46c63dd1490779350da51
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 15:55:35 +0530

    feat: add sem.c

---
+++ sem.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    FILE*fp;
+    char ch;
+    int roll_no;
+    //clrscr();
+    fp=fopen("student.txt","r");
+    if (fp==NULL)
+    {
+        printf("file could not open:");
+        exit(1);
+    }
+    do
+    {
+        ch=fgetc(fp);
+        putch(ch);
+
+    } while (ch!=EOF);
+    
+
+    fclose(fp);
+   // getch();
+    
+
+}
