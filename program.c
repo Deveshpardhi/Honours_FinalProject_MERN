3cca1866fcf64d7ab8dacb7ac66d3d48f2566661
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-07-09 15:55:33 +0530

    feat: add program.c

---
+++ program.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int age=18;
+    float PI=3.14;
+    char  star='*';
+
+     printf("age is %d \n",age);
+     printf("PI is %f \n",PI);
+     printf("star is %c \n",star);
+
+    return 0;
+}
